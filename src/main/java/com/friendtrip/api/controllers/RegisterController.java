16d576b4435b27df93dd9339324b435f82819fa9
lfernandosantos <lf.fernandodossantos@MacBook-Fernando.local>
2018-11-20 19:16:30 -0200

    Check if user already registered.

--- src/main/java/com/friendtrip/api/controllers/RegisterController.java
+++ src/main/java/com/friendtrip/api/controllers/RegisterController.java
@@ -2,6 +2,7 @@ package com.friendtrip.api.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.friendtrip.api.dto.CoordinateDto;
 import com.friendtrip.api.dto.TripDto;
 import com.friendtrip.api.dto.UserDto;
+import com.friendtrip.api.entities.Trip;
 import com.friendtrip.api.entities.User;
+import com.friendtrip.api.repositories.RepositoryManager;
+import com.friendtrip.api.repositories.TripRepository;
 import com.friendtrip.api.repositories.UserRepository;
 import com.friendtrip.api.responses.Response;
+import com.friendtrip.api.utils.ConstantMessages;
 import com.friendtrip.api.utils.ConstantPaths;
 
 @RestController
@@ -30,34 +36,55 @@ public class RegisterController {
 	@Autowired
 	UserRepository userRepository;
 	
+	@Autowired
+	TripRepository tripRepository;
+	
+	
 	@PostMapping(ConstantPaths.API.URL_REGISTER)
-	public ResponseEntity<Response<UserDto>> registerUser(@Valid @RequestBody UserDto user, BindingResult result) {
+	public ResponseEntity<Response<UserDto>> registerUser(@Valid String valid, @RequestBody UserDto user, BindingResult result) {
 		
 		Response<UserDto> response = new Response<>();
-		
 		if( result.hasErrors()) {
 			result.getAllErrors().forEach( error -> response.getErrors().add(error.getDefaultMessage()));
 			return ResponseEntity.badRequest().body(response);
-		} else {
+		} 
+		
+		//repositoryManager.saveUser(user);
+		
+			User isSaved = userRepository.findByFacebookID(user.getFacebookID());
+			
+			System.out.println(isSaved);
+			if(isSaved != null) {
+				response.setStatusCode(ConstantMessages.Response.StatusCode.ALREADY_REGISTERED);
+				response.setMsg(ConstantMessages.Response.Message.ALREADY_SAVE);
+			
+				return ResponseEntity.ok(response);	
+			}
 			
 			User userEntity = new User();
-			
 			userEntity.setFacebookID(user.getFacebookID());
 			userEntity.setName(user.getName());
-			System.out.println(userEntity.toString());
 			
 			userRepository.save(userEntity);
+//			
+//			System.out.println("printando");
+//			repositoryManager = new RepositoryManager();
+//			User user2 = repositoryManager.getUser(user.getFacebookID());
+//			
+//			System.out.println(user);
+//			
+			User alreadySaved = userRepository.findByFacebookID(user.getFacebookID());
 			
-			User userSaved = userRepository.findByFacebookID(user.getFacebookID());
-			
+			User userSaved = alreadySaved;
 			user.setId(userSaved.getId());
 			user.setFacebookID(userSaved.getFacebookID());
 			user.setName(userSaved.getName());
-			
+
+			response.setStatusCode(ConstantMessages.Response.StatusCode.SAVED_SUCCESSFULLY);
+			response.setMsg(ConstantMessages.Response.Message.SAVED_SUCCESS);
 			response.setData(user);
 			
 			return ResponseEntity.ok(response);
-		}
 	}
 	
 	@PostMapping(ConstantPaths.API.URL_CREATE_TRIP)
@@ -68,32 +95,56 @@ public class RegisterController {
 			result.getAllErrors().forEach( error -> response.getErrors().add(error.getDefaultMessage()));
 			return ResponseEntity.badRequest().body(response);
 		}
-		trip.setId(1L);
+		
+		Trip tripEntity = new Trip();
+		tripEntity.setName(trip.getName());
+		tripEntity.setDate(trip.getDate());
+		tripEntity.setDescription(trip.getDescription());
+		tripEntity.setLatitude(trip.getLatitude());
+		tripEntity.setLongitude(trip.getLongitude());
+		tripEntity.setStatus(trip.getStatus());
+		tripEntity.setCreator(trip.getCreator());
+		
+		//Optional<User> user = this.userRepository.findById(trip.getIdCreator());
+		//tripEntity.setCreator(user.get());
+		
+		this.tripRepository.save(tripEntity);
+		
+		Trip tripSaved = this.tripRepository.findByName(tripEntity.getName());
+		
+		trip.setId(tripSaved.getId());
 		response.setData(trip);
 		
 		return ResponseEntity.ok(response);
 	}
 	
+	
 	@PatchMapping(ConstantPaths.API.URL_GET_TRIPS_COORDINATORS)
-	public ResponseEntity<Response<List<TripDto>>> getTripsForLatLon(@Valid @RequestBody CoordinateDto coordinateDto, BindingResult result) {
+	public ResponseEntity<Response<List<Trip>>> getTripsForLatLon(@Valid @RequestBody CoordinateDto coordinateDto, BindingResult result) {
 		
-		Response<List<TripDto>> response = new Response<>();
+		Response<List<Trip>> response = new Response<>();
 		if(result.hasErrors()) {
 			result.getAllErrors().forEach( error -> response.getErrors().add(error.getDefaultMessage()));
 			return ResponseEntity.badRequest().body(response);
 		}
 		
-		TripDto trip = new TripDto();
-		TripDto trip2 = new TripDto();
+		List<Trip> findAll = this.tripRepository.findAll();
 		
-		trip.setName("Festa Niver");
-		trip2.setName("Corrida na praia");
+		response.setData(findAll);
+		return ResponseEntity.ok(response);
+	}
+	
+	@GetMapping(value = ConstantPaths.API.URL_GET_TRIPS_FOR_USER+"/{facebookid}")
+	public ResponseEntity<Response<List<Trip>>> getTripsForUser(@PathVariable("facebookid") String id) {
+		Response<List<Trip>> response = new Response<>();
+//		if(result.hasErrors()) {
+//			result.getAllErrors().forEach( error -> response.getErrors().add(error.getDefaultMessage()));
+//			return ResponseEntity.badRequest().body(response);
+//		}
 		
-		List<TripDto> trips = new ArrayList<TripDto>();
-		trips.add(trip);
-		trips.add(trip2);
+		List<Trip> findAll = this.tripRepository.findByCreator(id);
 		
-		response.setData(trips);
+		response.setData(findAll);
 		return ResponseEntity.ok(response);
 	}
 
